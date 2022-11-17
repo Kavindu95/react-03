1e8229de13bde4d83b1789b34a61ab8fd54a780c
Kavindu95 <kavindueng95@gmail.com>
2022-11-17 13:59:08 +0530

    project added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MainContent from './Component/MainContent';
+import Header from './Component/Header';
+import Footer from './Component/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <MainContent/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
